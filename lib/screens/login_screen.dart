74a667b659f246a8d610265dad6e6bd303c083dd
Yashas H Majmudar <yashashm@gmail.com>
2023-02-03 13:09:51 +0530

    ios changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -26,142 +26,146 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
-    return Scaffold(
-      appBar: CustomAppBar.customAppBar(
-        'Login',
-        backEnabled: false,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(13.sp),
-        height: screenSize.height,
-        width: screenSize.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 150.sp,
-              width: 150.sp,
-              margin: EdgeInsets.only(
-                top: 50.sp,
-                bottom: 80.sp,
-              ),
-              padding: EdgeInsets.all(15.sp),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(25.sp)),
-                color: const Color(0xFFF6F5E8),
-              ),
-              child: SvgPicture.asset(
-                AppConstants.appLogo,
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Phone Number',
-                counterText: '',
-                labelStyle: TextStyle(color: AppColors().darkGreen),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.sp),
-                  borderSide: BorderSide(
-                    width: 2.sp,
-                    color: AppColors().darkGreen,
-                  ),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: CustomAppBar.customAppBar(
+          'Login',
+          backEnabled: false,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(13.sp),
+          height: screenSize.height,
+          width: screenSize.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 150.sp,
+                width: 150.sp,
+                margin: EdgeInsets.only(
+                  top: 50.sp,
+                  bottom: 80.sp,
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.sp),
-                  borderSide: BorderSide(
-                    width: 2.sp,
-                    color: AppColors().darkGreen,
-                  ),
+                padding: EdgeInsets.all(15.sp),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25.sp)),
+                  color: const Color(0xFFF6F5E8),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.sp),
-                  borderSide: BorderSide(
-                    width: 2.sp,
-                    color: AppColors().darkGreen,
-                  ),
+                child: SvgPicture.asset(
+                  AppConstants.appLogo,
                 ),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.sp),
-                  borderSide: BorderSide(
-                    width: 2.sp,
-                    color: Colors.redAccent,
-                  ),
-                ),
-                filled: true,
-                fillColor: Colors.white,
               ),
-              style: TextStyle(
-                fontSize: 18.sp,
-                color: AppColors().darkGreen,
-              ),
-              keyboardType: TextInputType.number,
-              maxLines: 1,
-              maxLength: 10,
-              textInputAction: TextInputAction.done,
-              onChanged: (value) => number = value,
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(13.sp),
-                    color: login ? Colors.black38 : AppColors().darkGreen,
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  counterText: '',
+                  labelStyle: TextStyle(color: AppColors().darkGreen),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.sp),
+                    borderSide: BorderSide(
+                      width: 2.sp,
+                      color: AppColors().darkGreen,
+                    ),
                   ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: () async {
-                        setState(() {
-                          login = true;
-                        });
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.sp),
+                    borderSide: BorderSide(
+                      width: 2.sp,
+                      color: AppColors().darkGreen,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.sp),
+                    borderSide: BorderSide(
+                      width: 2.sp,
+                      color: AppColors().darkGreen,
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.sp),
+                    borderSide: BorderSide(
+                      width: 2.sp,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  color: AppColors().darkGreen,
+                ),
+                keyboardType: TextInputType.number,
+                maxLines: 1,
+                maxLength: 10,
+                textInputAction: TextInputAction.done,
+                onChanged: (value) => number = value,
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13.sp),
+                      color: login ? Colors.black38 : AppColors().darkGreen,
+                    ),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () async {
+                          setState(() {
+                            login = true;
+                          });
 
-                        bool result = false;
+                          bool result = false;
 
-                        if (number == '120223') {
-                          AppSharedPreferences.setLoggedIn = true;
-                          AppSharedPreferences.setLoginNumber = number;
-                          final box = Get.find<BoxController>();
-                          await box.getFamilyData();
+                          if (number == '120223') {
+                            AppSharedPreferences.setLoggedIn = true;
+                            AppSharedPreferences.setLoginNumber = number;
+                            final box = Get.find<BoxController>();
+                            await box.getFamilyData();
 
-                          Get.off(() => CoordHomeScreen());
-                        } else {
-                          result = await FirebaseHelper().login(number);
-                        }
+                            Get.off(() => CoordHomeScreen());
+                          } else {
+                            result = await FirebaseHelper().login(number);
+                          }
 
-                        setState(() {
-                          login = false;
-                        });
+                          setState(() {
+                            login = false;
+                          });
 
-                        if (result) {
-                          Get.off(() => const OnBoardingScreen());
-                        }
-                      },
-                      splashColor: Colors.white,
-                      child: Container(
-                        height: 60.sp,
-                        width: 200.sp,
-                        alignment: Alignment.center,
-                        child: login
-                            ? const CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : Text(
-                                'Login',
-                                style: TextStyle(
+                          if (result) {
+                            Get.off(() => const OnBoardingScreen());
+                          }
+                        },
+                        splashColor: Colors.white,
+                        child: Container(
+                          height: 60.sp,
+                          width: 200.sp,
+                          alignment: Alignment.center,
+                          child: login
+                              ? const CircularProgressIndicator(
                                   color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 20.sp,
+                                )
+                              : Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20.sp,
+                                  ),
                                 ),
-                              ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
